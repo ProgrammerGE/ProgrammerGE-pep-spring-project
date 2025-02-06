dacb39d09c4b0a8f7432764be25088de93ce9750
ProgrammerGE <gabriel.evans@eagles.ewu.edu>
2025-02-06 19:09:49 +0000

    Added @Repository

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -3,9 +3,11 @@ package com.example.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.example.entity.Account;
 //Edited by Gabriel Evans
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Integer>{
 
     @Query("FROM ACCOUNT WHERE username = :userVar")
--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.example.entity.Message;
 //Edited by Gabriel Evans
+@Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
 
     @Query("FROM MESSAGE WHERE postedBy = :postedVar")
